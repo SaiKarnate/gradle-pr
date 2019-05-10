f20a5714651460e7f9158f5bc227b2f85381ca0e
skarnat <skarnat@l-hyd02cj9k8-m.gap.com>
2019-05-10 16:08:01 +0530

    project init (master)

---
+++ gradle-demo-data/src/main/java/com/example/gradle/demo/gradledemodata/GradleDemoDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle.demo.gradledemodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoDataApplication.class, args);
+	}
+
+}
